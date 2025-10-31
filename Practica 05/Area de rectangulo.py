d796aaa9f6d7012b4ae339f64b1fa55f6aca0fe0
rafay acosta <rafay_acosta@hotmail.com>
2025-10-31 13:51:26 -0700

    Practica Area de rectangulo

---
+++ Practica 05/Area de rectangulo.py
@@ -0,0 +1,8 @@
+print ("Calculemos el area del rectangulo")
+
+H = int(input("Ingresa la altura del triangulo: "))
+B = int(input("Ingrese la base del triangulo: "))
+
+area = (B * H)/2
+
+print("el Area del rectangulo es:", area)
