64c73d6f89a1efd30185db637004c36fbf106f85
rafay acosta <rafay_acosta@hotmail.com>
2025-11-29 20:21:34 -0700

    avences proyecto final

---
+++ Proyecto final/modelos/cancion.py
@@ -0,0 +1,26 @@
+from colorama import Fore, Style, init
+import pandas as pd
+
+def agregarCancion():
+    nombre = input("Nombre de la canción: ")
+    artista = input("Nombre del artista: ")
+    duracion = input("Duración: ") 
+    streams = int(input("Streams: "))
+    
+    cancion = {
+        "nombre": nombre,
+        "artista": artista,
+        "duracion": duracion,
+        "streams": streams
+    }
+    return cancion
+
+def mostrarCanciones(playlist):
+    print(Fore.LIGHTMAGENTA_EX + " ===Lista de canciones=== " + Style.BRIGHT)
+    for i, cancion in enumerate(playlist["canciones"]):
+        print(f"ID:{i} - {cancion["nombre"]} by {cancion["artista"]}, [{cancion["duracion"]}], [{cancion["streams"]}]")
+
+def eliminarCancion(playlist, idPlaylist):
+    playlist.pop(idPlaylist)
+    print(Fore.GREEN + "Canción eliminada de la playlist." + Style.RESET_ALL)
+    return playlist
